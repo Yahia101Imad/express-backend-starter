9f31925752cefff72ffb273aa39cc1458078bdc5
Yahia Imad Eddine <yahiaimadeddine1@gmail.com>
2026-06-14 13:15:27 +0100

    fix: mismatch port to match the .env

--- src/server.js
+++ src/server.js
@@ -5,7 +5,7 @@ import { env } from "./config/env.js";
 const startServer = async () => {
   await connectDB();
 
-  app.listen(env.port, () => {
+  app.listen(env.PORT, () => {
     console.log(
       `🚀 Server running on http://localhost:${env.PORT}`
     );
