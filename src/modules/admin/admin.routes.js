581d1b5756b8678fcf7abb2997244937ec273693
Yahia Imad Eddine <yahiaimadeddine1@gmail.com>
2026-06-18 19:16:30 +0100

    feat: add admin dashboard and user management permissions

--- src/modules/admin/admin.routes.js
+++ src/modules/admin/admin.routes.js
@@ -1,19 +1,30 @@
 import express from "express";
+import { getDashboard, getUsers, deleteUser } from "./admin.controller.js";
 import protect from "../../middlewares/auth.middleware.js";
 import allowPermissions from "../../middlewares/permission.middleware.js";
 import { PERMISSIONS } from "../../common/constants/permissions.js";
-import { getDashboard } from "./admin.controller.js";
 
 const router = express.Router();
 
 router.get(
   "/dashboard",
-
   protect,
-
   allowPermissions(PERMISSIONS.DASHBOARD_VIEW),
-
   getDashboard,
+);
+
+router.get(
+  "/users",
+  protect,
+  allowPermissions(PERMISSIONS.USER_READ),
+  getUsers,
+);
+
+router.delete(
+  "/users/:id",
+  protect,
+  allowPermissions(PERMISSIONS.USER_DELETE),
+  deleteUser,
 );
 
 export default router;
