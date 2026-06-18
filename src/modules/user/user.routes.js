77249d91accddaf8549ea5a640ef10a88d5a2bd1
Yahia Imad Eddine <yahiaimadeddine1@gmail.com>
2026-06-18 18:59:26 +0100

    refactor: remove unnecessary code and dead files

--- src/modules/user/user.routes.js
+++ src/modules/user/user.routes.js
@@ -1,26 +1,13 @@
 import express from "express";
 import protect from "../../middlewares/auth.middleware.js";
-import allowRoles from "../../middlewares/role.middleware.js";
 
 const router = express.Router();
 
-router.get("/profile", protect, (req, res) => {
-  res.json({
-    message: "Your profile",
-    user: req.user,
-  });
-});
-
-// for "admin" only
-router.get(
-  "/admin",
-  protect,
-  allowRoles("admin"),
-  (req, res) => {
-    res.json({
-      message: "Welcome admin",
-    });
-  }
-);
+// router.get("/profile", protect, (req, res) => {
+//   res.json({
+//     message: "Your profile",
+//     user: req.user,
+//   });
+// });
 
 export default router;
