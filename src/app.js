275922473b1a6223daf9cb4e2d3d8918a7aa8b4a
Yahia Imad Eddine <yahiaimadeddine1@gmail.com>
2026-06-19 10:56:09 +0100

    feat: add request size limit protection

--- src/app.js
+++ src/app.js
@@ -13,8 +13,19 @@ const app = express();
 app.use(helmet());
 app.use(globalLimiter);
 app.use(cors());
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+// app.use(express.json());
+app.use(
+  express.json({
+    limit: "10kb",
+  }),
+);
+// app.use(express.urlencoded({ extended: true }));
+app.use(
+  express.urlencoded({
+    extended: true,
+    limit: "10kb",
+  })
+);
 
 app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerSpec));
 
