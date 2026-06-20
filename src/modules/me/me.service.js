1920f4192a67d9b0989cdc955e6815559ca89a13
Yahia Imad Eddine <yahiaimadeddine1@gmail.com>
2026-06-20 10:27:23 +0100

    security(users): prevent updating restricted user fields

--- src/modules/me/me.service.js
+++ src/modules/me/me.service.js
@@ -1,7 +1,17 @@
 import User from "../user/user.model.js";
 
 export const updateMeService = async (userId, updateData) => {
-  const user = await User.findByIdAndUpdate(userId, updateData, {
+  const allowedFields = ["name", "email"];
+
+  const filteredData = {};
+
+  allowedFields.forEach((field) => {
+    if (updateData[field] !== undefined) {
+      filteredData[field] = updateData[field];
+    }
+  });
+
+  const user = await User.findByIdAndUpdate(userId, filteredData, {
     new: true,
     runValidators: true,
   }).select("-password");
